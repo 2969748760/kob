7f96ba46d8b8a31b356b85df1b86c6325ecac7c8
Zexi Zhong <2969748760@qq.com>
2022-11-05 23:22:00 +0800

    配置Mysql和注册登录模块(中)

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,57 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import $ from 'jquery'
 
 export default {
   components: {
     NavBar
+  },
+  setup() {
+    $.ajax({
+      url: "http://localhost:3000/user/account/token/",
+      type: "post",
+      data: {
+        username: 'zzx',
+        password: 'zzx20011214',
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer "+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJhMDY3NzQwMjgxMzY0OTc5YTljNjAzMmI5ZWRhMjZkMSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY2NzY1OTY2NiwiZXhwIjoxNjY4ODY5MjY2fQ.9bl2titCMuFR5c5UvLmagFpFTRjxwHsD6QmhF3AJPsg"
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/register/",
+      type: "post",
+      data: {
+        username: "zzx2",
+        password: "zzx220011214",
+        confirmedPassword: "zzx220011214",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
   }
 }
 </script>
